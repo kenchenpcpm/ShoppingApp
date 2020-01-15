5f5f7d72d7c443113dc42d21f4364eee048b9b6d
Ken Chen <kenchenpcpm@gmail.com>
2020-01-15 15:30:31 -0800

    Initial

---
+++ Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart
+{
+    class Account
+    {
+        public object Regex { get; private set; }
+        public string AccountName { get; set; }
+        public decimal AccountTotal { get; private set; }
+        public DateTime Date { get; set; }
+        public string ShoppingCart { get; set; }
+
+        public void AddItems()
+        {
+                        
+        }
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountName = "Ken's account";
+            myAccount.ShoppingCart = " \n";
+            myAccount.AddItems();
+
+            Console.WriteLine($"Account Name:  {myAccount.AccountName}, Total Items: {myAccount.AccountTotal }  ");
+        }
+    }
+}
